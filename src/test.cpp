86e56dfc0225bbd3075ba7381018e5de447eb2cc
josh <joshuaholman5@gmail.com>
2025-05-03 18:33:04 -0700

    setup complete

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LLVMContext.h"
 
 #include "test.hpp"
 
@@ -8,7 +8,7 @@ namespace test {
 
 void test_libs() {
   std::cout << "Hello from test_libs!" << std::endl;
-  // llvm::LLVMContext context;
-  // std::cout << "LLVMContext created!" << std::endl;
+  llvm::LLVMContext context;
+  std::cout << "LLVMContext created!" << std::endl;
 }
 } // namespace test
