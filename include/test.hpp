77d42818efb3ed7a0811c5874c8b342866077410
josh <joshuaholman5@gmail.com>
2025-05-03 17:14:38 -0700

    init

---
+++ include/test.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+namespace test {
+void hi(){std::cout << "hi!!\n"}
+
+} // namespace test
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "test.hpp"
+
+int main(int argc, char *argv[]) {
+  test::hi();
+  return 0;
+}
